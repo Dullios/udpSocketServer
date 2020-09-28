35e0124614ac6d62062d569e34da54c7075dea26
EC2 Default User <ec2-user@ip-172-31-18-179.us-east-2.compute.internal>
2020-09-28 17:55:15 +0000

    Server provides position information to client

--- server.py
+++ server.py
@@ -23,7 +23,8 @@ def connectionLoop(sock):
             clients[addr] = {}
             clients[addr]['lastBeat'] = datetime.now()
             clients[addr]['color'] = 0
-            message = {"cmd": 0, "id":str(addr), "color": {"R": 0.0, "G": 0.0, "B": 0.0}}
+            clients[addr]['position'] = {"x": random.randint(-3, 3), "y": random.randint(-3, 3), "z": random.randint(-1, 3)}
+            message = {"cmd": 0, "id":str(addr), "color": {"R": 0.0, "G": 0.0, "B": 0.0}, 'position': clients[addr]['position']}
             m = json.dumps(message)
             for c in clients:
                sock.sendto(bytes(m,'utf8'), (c[0],c[1]))
@@ -32,6 +33,7 @@ def connectionLoop(sock):
                player = {}
                player['id'] = str(c)
                player['color'] = clients[c]['color']
+               player['position'] = clients[c]['position']
                playerList['players'].append(player)
                l = json.dumps(playerList)
             sock.sendto(bytes(l, 'utf8'), (addr[0],addr[1]))
@@ -42,7 +44,7 @@ def cleanClients(sock):
          if (datetime.now() - clients[c]['lastBeat']).total_seconds() > 5:
             print('Dropped Client: ', c)
 
-            message = {"cmd": 2, "id": str(c), "color": clients[c]['color']}
+            message = {"cmd": 2, "id": str(c), "color": clients[c]['color'], "position": clients[c]['position']}
             
             clients_lock.acquire()
             del clients[c]
@@ -63,6 +65,7 @@ def gameLoop(sock):
          clients[c]['color'] = {"R": random.random(), "G": random.random(), "B": random.random()}
          player['id'] = str(c)
          player['color'] = clients[c]['color']
+         player['position'] = clients[c]['position']
          GameState['players'].append(player)
       s=json.dumps(GameState)
       print(s)
